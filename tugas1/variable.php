708837dbb292d6ddd8e53ffb5d82c1ea2bb3534d
fanani03 <afanfanani202@gmail.com>
2022-03-10 14:42:48 +0700

    penambahan break di file variable.php

--- tugas1/variable.php
+++ tugas1/variable.php
@@ -8,7 +8,7 @@ $penjumlahan = $b + $a;
 $pengurangan = $c - $a;
 $perkalian = $a * $b * $c;
 
-echo ("Jumlah dari $a ditambah dengan $b adalah $penjumlahan");
-echo ("Hasil dari $c dikurangi dengan $a adalah $pengurangan");
-echo ("Jumlah perkalian dari $a dikali $b dikali $c adalah $perkalian");
+echo ("Jumlah dari $a ditambah dengan $b adalah $penjumlahan<br>");
+echo ("Hasil dari $c dikurangi dengan $a adalah $pengurangan<br>");
+echo ("Jumlah perkalian dari $a dikali $b dikali $c adalah $perkalian<br>");
 ?>

319d5acc09cfc27d48f36d007f794b03793e2992
fanani03 <afanfanani202@gmail.com>
2022-03-10 14:40:22 +0700

    Menambah isi dari file variable.php

---
+++ tugas1/variable.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 4;
+$b = 5;
+$c = 6;
+
+$penjumlahan = $b + $a;
+$pengurangan = $c - $a;
+$perkalian = $a * $b * $c;
+
+echo ("Jumlah dari $a ditambah dengan $b adalah $penjumlahan");
+echo ("Hasil dari $c dikurangi dengan $a adalah $pengurangan");
+echo ("Jumlah perkalian dari $a dikali $b dikali $c adalah $perkalian");
+?>

658209013f596934d93baf673ea59bc6ed86b603
fanani03 <afanfanani202@gmail.com>
2022-04-17 09:36:41 +0700

    Menambahkan file modul 2 acc.php

---
+++ praktikum/modul2/acc.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Ahmad Fanani/200411100143<br>";
+
+function tambah($x,$y) {
+    return $x + $y ;
+}
+
+
+
+function kali($x,$y) {
+    return $x * $y ;
+}
+
+
+function bagi($x,$y) {
+    return $x / $y ;
+}
+
+function bagi2($x,$y) {
+    return $x / $y ;
+}
+
+echo "Hasil x + y = ". tambah(4,5). "<br>";
+
+echo "Hasil x * y = ". kali(4,5). "<br>";
+
+echo "Hasil x / y = ". bagi(4,5);
+
+
+?>
